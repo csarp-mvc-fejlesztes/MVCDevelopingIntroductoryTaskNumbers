fba40e734200178f50e390fd5df310255a6e8e49
Csaba Gyuris <gycsaba@vasvari.hu>
2019-04-04 12:26:51 +0200

    Kiinduló projekt saját fejlesztéshez

---
+++ NumbersProjekt/NumbersProjekt/view/NumbersForm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace NumbersProjekt
+{
+    public partial class NumbersForm : Form
+    {
+        public NumbersForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }     
+    }
+}
