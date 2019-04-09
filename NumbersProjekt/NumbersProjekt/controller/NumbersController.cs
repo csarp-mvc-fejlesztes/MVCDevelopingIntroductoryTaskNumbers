db2a78a0c436683dedb3c548f1fd4bc630e92191
Csaba Gyuris <gycsaba@vasvari.hu>
2019-04-09 13:27:02 +0200

    NumbersController osztály a controller rétegben

---
+++ NumbersProjekt/NumbersProjekt/controller/NumbersController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumbersProjekt.controller
+{
+    /// <summary>
+    /// Feladata:
+    ///  - az adattárban lévő adatok elérése, kezelése
+    ///  - az outputra szánt adatok átalakítása a megfeleő formátumba
+    ///  - input adatok ellenörzése
+    ///  - hibaüzenetek megjelenítése a logba vagy a felhasználó felé (kivételdobás)
+    /// </summary>
+    class NumbersController
+    {
+    }
+}
