2ab18973a9374f563d4b1ac68d4af4289eb6a5d3
Csaba Gyuris <gycsaba@vasvari.hu>
2019-04-09 10:34:03 +0200

    Számok listája a repoban, példányosítás és getter.

--- NumbersProjekt/NumbersProjekt/repository/NumbersRepository.cs
+++ NumbersProjekt/NumbersProjekt/repository/NumbersRepository.cs
@@ -8,5 +8,23 @@ namespace NumbersProjekt.repository
 {
     class NumbersRepository
     {
+        private List<double> numbers;
+
+        /// <summary>
+        /// Konstruktorban példányosítás
+        /// </summary>
+        public NumbersRepository()
+        {
+            numbers = new List<double>();
+        }
+
+        /// <summary>
+        /// Getter
+        /// </summary>
+        /// <returns>Számok listája</returns>
+        public List<double> getNumbers()
+        {
+            return numbers;
+        }
     }
 }
