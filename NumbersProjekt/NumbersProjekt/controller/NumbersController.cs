acdaa1d270d5996514dc0e6eb8c2e81487af1f0b
Csaba Gyuris <gycsaba@vasvari.hu>
2019-04-10 08:24:26 +0200

    Input átalakítások hozzáadás és módosításnál valamint törlés

--- NumbersProjekt/NumbersProjekt/controller/NumbersController.cs
+++ NumbersProjekt/NumbersProjekt/controller/NumbersController.cs
@@ -27,7 +27,7 @@ namespace NumbersProjekt.controller
         /// <summary>
         /// Output átalakítás:
         ///  - eröltetett példa, a számok listájából stringek listáját készíti el
-        ///  - bemutatja a példa az output szánt átalakítást
+        ///  - bemutatja a példa az outputra szánt átalakításokat
         /// </summary>
         /// <returns>Számok listája</returns>
         public List<string> getNumbers()
@@ -39,6 +39,40 @@ namespace NumbersProjekt.controller
                 numbersString.Add(number.ToString());
             }
             return numbersString;
+        }
+        
+        /// <summary>
+        /// Input átalakítások:
+        /// A viewn megadott szöveget számmá konvertálja és a reposiotry segitségével eltárolja
+        /// </summary>
+        /// <param name="numberText">A felhasználó által megadott szám</param>
+        public void addNumber(string numberText)
+        {
+            double number = 0;
+            number = Convert.ToDouble(numberText);
+            nr.add(number);
+        }
+
+        /// <summary>
+        /// A viewn megadott módosított értéket számma konvertálja és a repository segítségével módosítja
+        /// az indexedit elemet az új számra
+        /// </summary>
+        /// <param name="index">A sorszám, amely megadja, hogy hanyadik elemet kell változtatni</param>
+        /// <param name="numberText">A módosított szám új értéke</param>
+        public void modifyNumber(int index, string numberText)
+        {
+            double number = 0;
+            number = Convert.ToDouble(numberText);
+            nr.modify(index, number);
+        }
+
+        /// <summary>
+        /// Indexedit elem törlése a repository segítségével
+        /// </summary>
+        /// <param name="index">Annak az elemnek a sorszáma a melyet törölni kell</param>
+        public void deleteNumber(int index)
+        {
+            nr.remove(index);
         }
     }
 }
