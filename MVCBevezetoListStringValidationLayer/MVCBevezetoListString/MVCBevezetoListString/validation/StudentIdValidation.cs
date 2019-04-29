93f018572969c669abbada03005a4597ef53788c
Csaba Gyuris <gycsaba@vasvari.hu>
2019-04-29 12:10:15 +0200

    Repository rétegben List<string> és validation réteg

---
+++ MVCBevezetoListStringValidationLayer/MVCBevezetoListString/MVCBevezetoListString/validation/StudentIdValidation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCBevezetoListString.validation
+{
+    class StudentIdValidation
+    {
+        private string studentID;
+
+        public StudentIdValidation(string studentID)
+        {
+            this.studentID = studentID;
+        }
+
+        public void validation()
+        {
+            if (testStudendIdIsWrong())
+                throw new StudentIdValidationException(
+                    "Rossz a diak azonosító!");
+        }
+
+        /// <summary>
+        /// Teszteli a studend ID-t
+        /// </summary>
+        /// <returns>true, ha a student ID rossz</returns>
+        private bool testStudendIdIsWrong()
+        {
+            
+        }
+    }
+}
---
+++ MVCBevezetoListStringValidationLayer/MVCBevezetoListString/MVCBevezetoListString/validation/StudentIdValidationException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace MVCBevezetoListString.validation
+{
+    [Serializable]
+    internal class StudentIdValidationException : Exception
+    {
+        public StudentIdValidationException()
+        {
+        }
+
+        public StudentIdValidationException(string message) : base(message)
+        {
+        }
+
+        public StudentIdValidationException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected StudentIdValidationException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
