ea732d4cf942360a4ce1dd3a5a2886f6f1b85b9a
Csaba Gyuris <gycsaba@vasvari.hu>
2019-04-29 12:09:17 +0200

    Kiinduló projekt

---
+++ MVCBevezetoListStringValidationLayer/MVCBevezetoListString/MVCBevezetoListString/controller/ControllerStudentID.cs
@@ -0,0 +1,29 @@
+﻿using MVCBevezetoListString.repository;
+using MVCBevezetoListString.validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCBevezetoListString.controller
+{
+    class ControllerStudentID
+    {
+        private RepositoryStudentID repo;
+
+        public void add(string studendId)
+        {
+            try
+            {
+                StudentIdValidation sv;
+                sv = new StudentIdValidation(studendId);
+                sv.validation();
+            }
+            catch(StudentIdValidationException sve)
+            {}
+            catch(Exception e)
+            {}
+        }
+    }
+}
